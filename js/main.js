a7c8b4abb38babd26bb4fb977024c96730dff542
Oksil-Joo <jueun1125!>
2021-03-24 21:12:33 -0400

    update drag

--- js/main.js
+++ js/main.js
@@ -2,4 +2,31 @@
 (() => {
   // select and work with our audio element
   console.log('fired!');
-})()
+
+  const playZone = document.querySelector("#playZone"),
+        dragImages = document.querySelectorAll(".tapeImages");
+
+function dragStart() {
+  console.log('sgarted draggin');
+}
+
+function draggedOver(event) {
+  event.preventDefault();
+  console.log('dragged over me');
+}
+
+function dropped(event) {
+  event.preventDefault();
+  console.log(dragged over me');
+}
+
+function dropped(event) {
+  event.preventDefault();
+  console.log('dropped somethin on me');
+}
+
+dragImages.addEventListener("dragstart", dragStart);
+dropZones.addEventListener("dragover", draggedOver);
+dropZones.addEventListener("drop", dropped);
+
+})();
